66d32d9a7b7cec1031dc7eb5dcdfcc0f26c8d407
AlaeCode90 <alaa.lgatt@gmail.com>
2023-03-16 07:16:29 -0700

    Hello,puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
-/*
- * main - function to print out  
- * Return 0 if success
+/**
+ * Description: main - Prints out a string to stdout.
+ * Return: 0 if success.
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
-
