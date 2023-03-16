b1437eb0c40386579eee5997bafca462668f0754
AlaeCode90 <alaa.lgatt@gmail.com>
2023-03-16 07:26:08 -0700

    5. Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - a function that prints out a string to stdout.
+ * Return: 0 if success.
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
