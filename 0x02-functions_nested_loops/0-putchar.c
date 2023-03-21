cea36358d2539b063bb7168f7c76e15ca57050b5
AlaeCode90 <alaa.lgatt@gmail.com>
2023-03-21 04:59:59 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,30 @@
 #include "main.h"
 
-int main(void)
+/**
+ * print_putchar - Prints the string "_putchar" using _putchar
+ *
+ * Return: void
+ */
+void print_putchar(void)
 {
-    print_putchar();
-    return (0);
+    _putchar('_');
+    _putchar('p');
+    _putchar('u');
+    _putchar('t');
+    _putchar('c');
+    _putchar('h');
+    _putchar('a');
+    _putchar('r');
+    _putchar('\n');
+}
+
+/**
+ * _putchar - Writes a character to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1, otherwise -1
+ */
+int _putchar(char c)
+{
+    return (write(1, &c, 1));
 }
