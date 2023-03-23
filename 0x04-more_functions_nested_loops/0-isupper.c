0c66f2b2b8cb1e5c0553eff27ff615d52f17335e
AlaeCode90 <alaa.lgatt@gmail.com>
2023-03-23 07:28:23 -0700

    0. isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+# include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - Entry point
+* 
+* return - always 0 if (success)
+*/
+
+int main(void)
+{
+char c
+if 
+{
+for ( c = 'A' ; c <= 'Z' ; c++)
+putchar(c);
+return (1);
+}
+else 
+return(0);
+}
