05b3778eb63d69d7bbcaea5e70296d01e83ce34b
AlaeCode90 <alaa.lgatt@gmail.com>
2023-03-23 17:26:25 -0700

    3. Leap year

---
+++ 0x03-debugging/3-main_b.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - takes a date and print how many days are left in the year, taking
+ * leap years intpo account
+ * return - always 0 if (success)
+ */
+
+int main(void)
+{
+int day;
+int month;
+int year;
+
+month = 2;
+day = 29;
+year = 2000;
+
+printf("Date : %02d%02d%04d\n", month, day, year);
+day = convert_day(month,day);
+
+print_remaining_days(2,28,2000);
+print_remaining_days(1,1,2000);
+
+return(0);
+}
